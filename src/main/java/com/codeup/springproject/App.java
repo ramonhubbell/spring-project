809d9da8f1d74a67f10ff0e91483c945a504d5c2
Ramon Hubbell <Ramon.hubbell@gmail.com>
2020-08-14 18:43:10 -0500

    feature: cleaned out commented code

--- src/main/java/com/codeup/springproject/App.java
+++ src/main/java/com/codeup/springproject/App.java
@@ -14,22 +14,6 @@ public class App {
 
     public static final String POSTS_API_URL = "https://www.olo.com/pizzas.json";
 
-//    static <K,V extends Comparable<? super V>>
-//    List<Map.Entry<K, V>> entriesSortedByValues(Map<K,V> map) {
-//
-//        List<Map.Entry<K,V>> sortedEntries = new ArrayList<Map.Entry<K,V>>(map.entrySet());
-//
-//        Collections.sort(sortedEntries,
-//                new Comparator<Map.Entry<K,V>>() {
-//                    @Override
-//                    public int compare(Map.Entry<K,V> e1, Map.Entry<K,V> e2) {
-//                        return e2.getValue().compareTo(e1.getValue());
-//                    }
-//                }
-//        );
-//
-//        return sortedEntries;
-
     public static void main(String[] args) throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
@@ -41,17 +25,6 @@ public class App {
 
         ObjectMapper mapper = new ObjectMapper();
         List<Combination> combinations = mapper.readValue(response.body(), new TypeReference<List<Combination>>() {});
-//        combinations.forEach(System.out::println);
-//        System.out.println(combinations.get(0).getToppings());
-//        System.out.println(combinations.get(1).getToppings());
-//        System.out.println(combinations.get(4).getToppings());
-//        for (int i = 0; i < combinations.size(); i += 1) {
-//            System.out.println(combinations.get(i).getToppings());
-//        }
-//        Map<String, Integer> combinationAndCount = new HashMap<>();
-//        for(combination : combinations) {
-//            Integer count = combinationAndCount.get(combination)
-//        }
         Map<List<String>, Integer> combinationAndCount = new HashMap<>();
         for (int i = 0; i < combinations.size(); i += 1) {
 //            System.out.println(combinations.get(i).getToppings());
@@ -70,19 +43,8 @@ public class App {
 
         System.out.printf("Reverse Sorted Map: %s \n", reverseSortedMap);
 
-
-
     }
-
-
-
-
-        }
-//        for (String language : languages) {
-//            System.out.println(language);
-//        }
-
-//        System.out.println(response.body());
+}
 
 
 
